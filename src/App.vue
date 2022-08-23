ef517919198aba92e4f5f12772e9cf8a4096cf06
dasha <smirnova032001@gmail.com>
2022-08-23 22:20:10 +0300

    learn vue

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,27 @@
+<script lang="ts">
+//import Vue from 'vue';
+import { Component, Vue } from 'vue-property-decorator';
+import { Header } from '@/components'
+
+@Component ({
+    components: {
+
+    }
+
+    prop
+})
+export default class Test extends Vue {
+  message: string = 'Films'
+}
+
+</script>
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view />
+    {{ message }}
   </div>
+
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+//<style lang="scss">
+//  
+//</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,14 @@
+<template>
+<h1>Header</h1>
+</template>
+
+<script lang="ts">
+//import Header from './Header.vue';
+//import Component from 'vue-property-decorator';
+import Vue from 'vue';
+
+export default class Header extends Vue {
+    name = 'Header'
+}
+
+</script>
